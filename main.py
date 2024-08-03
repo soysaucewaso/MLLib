b8d1666f38f917b55938cdfcb514df32db63375e
waso <sblankenship2507@sdsu.edu>
2024-08-03 08:46:53 -0700

    have a working prototype that can forward and backward propagate. Need to test with more than 1 layer

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import numpy as np
-def sigmoid(values: [[int]]):
+def sigmoid(values: [[int]]) -> np.ndarray:
     # logistic function
     # 1 / (1+e^x)
+    values = np.clip(values,-300,300)
     expValues = np.exp(-values)
     return 1 / (1+expValues)
 
 def siggrad(values: [[int]]):
     s = sigmoid(values)
     return s * (1-s)
+
+def tanh(values):
+    return np.tanh(values)
+
+def tanhgrad(values):
+    return 1 - tanh(values) ** 2
 def crossEntropyCost(predicted: [[int]], actual : [[int]]):
-    #loss = yln(a) + (1-y)(1-ln(a))
+    predicted = np.array(predicted)
+    actual = np.array(actual)
+    #loss = - yln(a) - (1-y)(ln(1-a))
     # cost function for binary classification
-    # cost = - loss / m
-    loss = (actual * np.log(predicted)) + ((1-actual) * np.log(1-predicted))
-    m = predicted.shape[1]
-    cost = np.sum(loss,keepdims=True) / (-m)
+    # cost = loss / m
+    m = predicted.shape[0]
+    try:
+        loss = - (actual * np.log(predicted)) - ((1-actual) * np.log(1-predicted))
+        cost = np.sum(loss, keepdims=True) / (m)
+    except:
+        loss = None
+        cost = np.sum(loss, keepdims=True) / (m)
+
+
     # grads
     grads = predicted - actual
     denom = predicted * (1-predicted)
@@ -30,7 +45,10 @@ class layer:
     y = wx + b
     """
     WEIGHT_INIT_SCALAR = .01
-    def __init__(self, amtNeurons: int, activation: str,prev:'layer'=None):
+    SIGMOID = [sigmoid,siggrad]
+    TANH = [tanh,tanhgrad]
+
+    def __init__(self, amtNeurons: int, activation,prev:'layer'=None):
         """
         :param amtNeurons: size of the layer
         :param activation: activation function used to transform output
@@ -41,8 +59,8 @@ class layer:
         activation: a function which maps a [[int]] to another [[int]] of the same size
         """
         self.amtNeurons = amtNeurons
-        self.activation = sigmoid
-        self.actgrad = siggrad
+        self.activation = activation
+
         self.prev: 'layer' = prev
         self.next: 'layer' = None
         self.weights, self.biases = None, None
@@ -52,7 +70,7 @@ class layer:
             self.initWeights(prev.amtNeurons)
     def append(self, amtNeurons: int, activation: str):
         nextLayer = layer(amtNeurons,activation,prev=self)
-        nextLayer.initWeights(amtNeurons)
+        nextLayer.initWeights(self.amtNeurons)
         if self.next is not None:
             self.next.prev = nextLayer
             self.next.initWeights(nextLayer.amtNeurons)
@@ -67,8 +85,8 @@ class layer:
         if inputSize == 0:
             return
         # randomly init to prevent neurons all getting the same weights
-        self.weights = np.random.randn(self.amtNeurons,inputSize) * layer.WEIGHT_INIT_SCALAR
-        self.biases = np.zeros((self.amtNeurons,1))
+        self.weights = np.random.randn(inputSize, self.amtNeurons) * layer.WEIGHT_INIT_SCALAR
+        self.biases = np.zeros((1,self.amtNeurons))
 
     @staticmethod
     def multiple(amtNeurons: [int],activation:str,prev: 'layer' = None):
@@ -88,7 +106,7 @@ class layer:
 
 
 class model:
-    def __init__(self, rootLayer: layer, cost: str, numIterations: int = 100, learningRate: float = .1):
+    def __init__(self, rootLayer: layer, cost: str, learningRate: float = .1):
         """
         :param rootLayer: inputLayer
         :param num_iterations: how many times the model trains
@@ -102,45 +120,49 @@ class model:
         self.tailLayer: layer = self.rootLayer
         while self.tailLayer.next:
             self.tailLayer = self.tailLayer.next
-        self.numIterations: int= numIterations
         self.learningRate: int = learningRate
         self.cost = crossEntropyCost
 
 
-    def train(self, inputData: [[int]], outputData: [[int]]):
-
+    def train(self, inputData: [[int]], outputData: [[int]], numIterations: int = 100):
+        inputData = np.array(inputData)
+        outputData = np.array(outputData)
         #init
-        self.rootLayer.initWeights(np.shape(inputData)[1])
-        assert(np.shape(self.tailLayer.weights)[0] == np.shape(outputData)[0])
+        if self.rootLayer.weights is None:
+            self.rootLayer.initWeights(np.shape(inputData)[1])
+        assert(np.shape(self.tailLayer.weights)[1] == np.shape(outputData)[1])
         """
         :param inputData: training inputs; shape: (feature size, # of inputs
         :param outputData: training outpus; shape: (label size, # of inputs)
         """
-        for i in range(self.numIterations):
+        for i in range(numIterations):
             predictions = self.forwardPropagate(self.rootLayer, inputData, True)
+            if predictions[0][0] is None:
+                return False
             cost, grads = self.computeCost(predictions,outputData)
             self.backwardPropagate(self.tailLayer, grads)
+        return True
 
     def predict(self,inputData: [[int]]):
-        assert(np.shape(self.rootLayer.weights)[1] == np.shape(inputData)[0])
+        if self.rootLayer.weights is None:
+            return
+        assert(np.shape(np.shape(inputData)[1] == self.rootLayer.weights)[0])
         predictions = self.forwardPropagate(self.rootLayer,inputData,False)
         return predictions
 
     def forwardPropagate(self,currLayer: layer, inputData: [[int]], cacheData: bool):
         assert(currLayer)
-        assert(np.shape(currLayer.weights)[1] == np.shape(inputData)[0])
+        assert(np.shape(np.shape(inputData)[1] == currLayer.weights)[0])
         """
         propagates through a layer, by calculating the input to the next layer
         :return: predicted values for y, the final layer
         """
-        if cacheData:
-            currLayer.cache = inputData
+
         # linear transformation
-        transformed = np.dot(currLayer.weights, inputData)
-
-        outputData = currLayer.activation(transformed)
-        # cache some stuff
-
+        transformed = np.dot(inputData, currLayer.weights) + currLayer.biases
+        outputData = currLayer.activation[0](transformed)
+        if cacheData:
+            currLayer.cache = (inputData, outputData)
         if currLayer.next is None:
             return outputData
         else:
@@ -158,22 +180,25 @@ class model:
 
     def backwardPropagate(self,currLayer: layer, grads: [[int]]):
         assert(currLayer)
-        actgrads = currLayer.actgrad(grads)
+        x = currLayer.cache[0]
+        z = currLayer.cache[1]
+        actgrads = currLayer.activation[1](z)
         dZ = grads * actgrads
-        # dZ/dW = XT
-        XT = np.transpose(currLayer.cache)
-        dW = np.dot(dZ,XT)
+        # dZ/dW = X
+        dW = np.dot(dZ.T,x).T
         # dZ/dB = 1
-        shape = np.shape(dZ)
-        dB = np.sum(dZ,axis=1).reshape((shape[0],1))
-        # dZ/dX = WT dot dZ
-        WT = np.transpose(currLayer.weights)
-        dX = np.dot(WT,grads)
+        dB = np.sum(dZ,axis=0).reshape((1,-1))
 
         currLayer.cache = None
-        self.adjustWeights(currLayer,dW,dB)
         if currLayer.prev is None:
+            self.adjustWeights(currLayer, dW, dB)
             return
+
+        # dZ/dX = WT
+        WT = np.transpose(currLayer.weights)
+        dX = np.dot(dZ, WT)
+
+        self.adjustWeights(currLayer, dW, dB)
         self.backwardPropagate(currLayer.prev,dX)
 
     def adjustWeights(self,currLayer: layer, wGrads: [[int]], bGrads: [[int]]):
--- mainTest.py
+++ mainTest.py
@@ -1,3 +1,4 @@
+import random
 import unittest
 import collections
 collections.Callable = collections.abc.Callable
@@ -5,13 +6,12 @@ import main
 import numpy as np
 import numpy.testing as tst
 import matplotlib.pyplot as plt
-
+import tensorflow_datasets as tfds
 class TestMain(unittest.TestCase):
     def test_sigmoid(self):
         X = np.array([[1,2],[4,-1]])
         res = main.sigmoid(X)
         expected = np.array([[0.7310586,0.8807971],[0.9820138,0.2689414]])
-        print(res)
         tst.assert_almost_equal(res,expected)
     def test_cross_entropy_loss(self):
         A = np.array([[0.5002307,  0.49985831, 0.50023963]])
@@ -29,9 +29,77 @@ class TestMain(unittest.TestCase):
     def test_model(self):
         l = main.layer(3, 'sigmoid')
         m = main.model(l, 'crossEntropyCost')
-        m.train(np.array([[.5,.5],[-.5,.5]]),np.array([[1, 0],[1,0],[1,1]]))
-        pred = m.predict([[.5,.5],[.5,-.5]])
+        m.train(np.array([[-1,0],[0,1]]),np.array([[1, 0,1],[1,0,1]]))
+        pred = m.predict([[-.5,.5],[30,2]])
         print(pred)
+
+    def to_ndarray(self,dataset):
+        inputdata = np.array([np.array(e[0]).flatten() for e in dataset])
+        outputdata = np.reshape([np.array(e[1]) for e in dataset],(-1,1))
+        return inputdata, outputdata
+
+
+
+    def testWithRandomData(self):
+        l = main.layer(4, main.layer.TANH)
+        l.append(1,main.layer.SIGMOID)
+        m = main.model(l, 'crossEntropyCost', learningRate=.01)
+        n = 100
+        k = 10
+        outputdata = [[random.randint(0,1)] for _ in range(n)]
+        inputdata = [[random.random(),(r[0]*-2+1),random.random()] for r in outputdata]
+        testo = [[random.randint(0,1)] for _ in range(k)]
+        testi = [[random.random(),(r[0]*-2+1),random.random()] for r in testo]
+        m.train(inputdata,outputdata,numIterations=20)
+        p = m.predict(testi)
+        for i in range(k):
+            print(f"Input: {testi[i]}Predicted: {p[i]},Actual: {testo[i]}")
+
+    def test_with_dataset(self):
+        l = main.layer(1, 'sigmoid')
+        m = main.model(l,'crossEntropyCost',learningRate=.00001)
+        beans, info = tfds.load("beans",with_info=True, as_supervised=True,split=['train','test'])
+        inputdata, outputdata = self.to_ndarray(beans[0])
+        inputdata = inputdata / 255.0
+        outputdata = [[min(e[0],1)] for e in outputdata]
+        testidata, testodata = self.to_ndarray(beans[1])
+        p1 = m.predict(testidata)
+        m.train(inputdata,outputdata,2)
+
+        p2 = m.predict(testidata)
+        for i in range(20):
+            print(f"P1: {p1[i]}, P2: {p2[i]}, Actual: {testodata[i]}")
+
+
+    def test_simple(self):
+        l = main.layer(1,main.layer.TANH)
+        m = l.append(1,main.layer.TANH)
+        m.append(1,main.layer.SIGMOID)
+        n= 100
+        input = [[random.randint(0,1)] for _ in range(n)]
+        output = [i for i in input]
+        m = main.model(l,'crossEntropyCost',learningRate=1)
+        m.train(input,output)
+        testi = [[random.randint(0,1)] for _ in range(10)]
+        p = m.predict(testi)
+        for i, ival in enumerate(testi):
+            print(f"Input: {ival} Actual: {p[i]}")
+
+    def test_small(self):
+        l = main.layer(2,main.layer.TANH)
+        m = l.append(2,main.layer.SIGMOID)
+
+        #o = m.append(5,main.layer.TANH)
+        #m.append(1,main.layer.SIGMOID)
+        input = [[0],[1]]
+        output = [[0,1],[1,1]]
+        m = main.model(l,'crossEntropyCost',learningRate=.3)
+        m.train(input,output)
+        testi = [[0],[1]]
+        p = m.predict(testi)
+        for i, ival in enumerate(testi):
+            print(f"Input: {ival} Actual: {p[i]}")
+
 
 if __name__ == '__main':
     unittest.main()
